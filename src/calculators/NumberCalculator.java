52c8a74896ee4a29f61c0d80c02964be5bfb5396
Crstn <Crstn@192.168.1.36>
2013-03-29 18:34:51 +0000

    NumberCalculator  Anotations added to methods

--- src/calculators/NumberCalculator.java
+++ src/calculators/NumberCalculator.java
@@ -25,18 +25,22 @@ public class NumberCalculator extends Calculator {
         return new IntegerType(arg0.getValue() + arg1.getValue());
     }
     
+    @OperatorSymbol(symbol="-")
     public Type substract(DoubleType arg0, DoubleType arg1){
         return new DoubleType(arg0.getValue() - arg1.getValue());
     }
     
+    @OperatorSymbol(symbol="-")
     public Type substract(DoubleType arg0, IntegerType arg1){
         return new DoubleType(arg0.getValue() - arg1.getValue());
     }
     
+    @OperatorSymbol(symbol="-")
     public Type substract(IntegerType arg0, DoubleType arg1){
         return new DoubleType (arg0.getValue() - arg1.getValue());
     }
     
+    @OperatorSymbol(symbol="-")
     public Type substract(IntegerType arg0, IntegerType arg1){
         return new IntegerType(arg0.getValue() - arg1.getValue());
     }
